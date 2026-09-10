ea5d0e3b8743b95b7c2ca1530364b8edf115327d
Kibreab Kebede <kibreyo@gmail.com>
2026-09-10 17:58:58 +0300

    Implement scroll-linked 3D particle hero animation from prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ParticleBackground from './components/ParticleBackground';
+import ThreeParticleHero from './components/ThreeParticleHero';
 import Home from './pages/Home';
 import Extracurriculars from './pages/Extracurriculars';
 import IndependentProjects from './pages/IndependentProjects';
@@ -10,10 +11,11 @@ import Chatbot from './components/Chatbot';
 function App() {
   return (
     <Router>
-      <div className="relative min-h-screen text-white overflow-hidden selection:bg-white/30 selection:text-white">
+      <div className="relative min-h-screen text-white overflow-x-hidden selection:bg-white/30 selection:text-white">
         <ParticleBackground />
         <Navbar />
         <Chatbot />
+        <ThreeParticleHero />
         
         <Routes>
           <Route path="/" element={<Home />} />
